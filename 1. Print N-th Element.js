5fde81e5f6d0d9529067672207512d6c7a978bbd
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-28 21:46:43 +0300

    Update 1. Print N-th Element.js

--- 1. Print N-th Element.js
+++ 1. Print N-th Element.js
@@ -2,9 +2,7 @@ function printNthElement(arr) {
     let step = Number(arr.pop());
     let newArr = [];
     for (let i = 0; i < arr.length; i += step) {
-        // if(i = 0 && i==Number(step)){
         newArr.push(arr[i]);
-        // }
     }
     console.log(newArr.join(` `));
 
